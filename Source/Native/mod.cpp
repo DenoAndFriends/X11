22003277e184f03f5009f2f577c1fd57a080b479
ElectronicsArchiver <85485984+ElectronicsArchiver@users.noreply.github.com>
2022-08-07 00:00:20 -0400

    Type Change Resistance

--- Source/Native/mod.cpp
+++ Source/Native/mod.cpp
@@ -9,6 +9,80 @@ using std::cout;
 using std::endl;
 
 
+
+const Atom types [] {
+    XA_PRIMARY ,
+    XA_SECONDARY ,
+    XA_ARC ,
+    XA_ATOM ,
+    XA_BITMAP ,
+    XA_CARDINAL ,
+    XA_COLORMAP ,
+    XA_CURSOR ,
+    XA_CUT_BUFFER0 ,
+    XA_CUT_BUFFER1 ,
+    XA_CUT_BUFFER2 ,
+    XA_CUT_BUFFER3 ,
+    XA_CUT_BUFFER4 ,
+    XA_CUT_BUFFER5 ,
+    XA_CUT_BUFFER6 ,
+    XA_CUT_BUFFER7 ,
+    XA_DRAWABLE ,
+    XA_FONT ,
+    XA_INTEGER ,
+    XA_PIXMAP ,
+    XA_POINT ,
+    XA_RECTANGLE ,
+    XA_RESOURCE_MANAGER ,
+    XA_RGB_COLOR_MAP ,
+    XA_RGB_BEST_MAP ,
+    XA_RGB_BLUE_MAP ,
+    XA_RGB_DEFAULT_MAP ,
+    XA_RGB_GRAY_MAP ,
+    XA_RGB_GREEN_MAP ,
+    XA_RGB_RED_MAP ,
+    XA_STRING ,
+    XA_VISUALID ,
+    XA_WINDOW ,
+    XA_WM_COMMAND ,
+    XA_WM_HINTS ,
+    XA_WM_CLIENT_MACHINE ,
+    XA_WM_ICON_NAME ,
+    XA_WM_ICON_SIZE ,
+    XA_WM_NAME ,
+    XA_WM_NORMAL_HINTS ,
+    XA_WM_SIZE_HINTS ,
+    XA_WM_ZOOM_HINTS ,
+    XA_MIN_SPACE ,
+    XA_NORM_SPACE ,
+    XA_MAX_SPACE ,
+    XA_END_SPACE ,
+    XA_SUPERSCRIPT_X ,
+    XA_SUPERSCRIPT_Y ,
+    XA_SUBSCRIPT_X ,
+    XA_SUBSCRIPT_Y ,
+    XA_UNDERLINE_POSITION ,
+    XA_UNDERLINE_THICKNESS ,
+    XA_STRIKEOUT_ASCENT ,
+    XA_STRIKEOUT_DESCENT ,
+    XA_ITALIC_ANGLE ,
+    XA_X_HEIGHT ,
+    XA_QUAD_WIDTH ,
+    XA_WEIGHT ,
+    XA_POINT_SIZE ,
+    XA_RESOLUTION ,
+    XA_COPYRIGHT ,
+    XA_NOTICE ,
+    XA_FONT_NAME ,
+    XA_FAMILY_NAME ,
+    XA_FULL_NAME ,
+    XA_CAP_HEIGHT ,
+    XA_WM_CLASS ,
+    XA_WM_TRANSIENT_FOR ,
+    XA_LAST_PREDEFINED
+};
+
+
 extern "C" {
 
     Display * connect(){
@@ -26,7 +100,7 @@ extern "C" {
     char * windowProperty(
         Display * display ,
         Window window ,
-        Atom type ,
+        uint8_t typeIndex ,
         char * name
     ){
         unsigned long bytes;
@@ -34,6 +108,8 @@ extern "C" {
         unsigned char * value;
         Atom returnType;
         int format;
+
+        const auto type = types[typeIndex];
 
         Atom property = XInternAtom(display,name,false);
 
